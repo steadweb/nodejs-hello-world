377ddce9157ad1260907a891c5bd9c56a36834ba
Luke Steadman <ljsteadman@gmail.com>
2021-04-29 17:11:08 +0100

    Added base sequelize

---
+++ config/config.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    username: 'luke',
+    password: 'cassette',
+    database: 'toca.bookings',
+    host: '127.0.0.1',
+    port: 3306,
+    dialect: 'mysql',
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    port: 3306,
+    dialect: 'mysql',
+  }
+};
