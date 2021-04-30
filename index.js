90ca9a6d7fa67c07964c93f5fb7f3dce99e8c56b
Luke Steadman <ljsteadman@gmail.com>
2021-04-30 17:39:13 +0100

    Reverted back to hello world

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, Luke!');
+  res.send('Hello World!');
 });
 
 module.exports.handler = serverless(app);
