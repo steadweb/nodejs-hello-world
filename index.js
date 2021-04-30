ede6035a165a03da4b1d300620f42ce9188debd0
Luke Steadman <ljsteadman@gmail.com>
2021-04-30 16:27:22 +0100

    Attempting trigger

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, Luke...');
+  res.send('Hello, Luke!');
 });
 
 module.exports.handler = serverless(app);
